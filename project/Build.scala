5f779a896fc0b58255c53510a8d46b42b26064ad
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-05-12 22:46:05 +0400

    computer database sample

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,18 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "playJavaGB"
-  val appVersion      = "1.0-SNAPSHOT"
+    val appName         = "computer-database"
+    val appVersion      = "1.0"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    javaCore,
-    javaJdbc,
-    javaEbean
-  )
+    val appDependencies = Seq(
+      javaCore,
+      javaJdbc,
+      javaEbean
+    )
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+      // Add your own project settings here      
+    )
 
 }
+            
